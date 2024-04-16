693483fa5dfc833cef703b56aaccf0618d99c829
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-04-16 11:18:08 +0800

    Time: 0 ms (100%), Space: 5.8 MB (51.1%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.c
@@ -0,0 +1,18 @@
+#include <string.h>
+char* longestCommonPrefix(char** strs, int strsSize) {
+    if (strsSize <= 1) return strs[strsSize-1];
+    int len_s = strlen(strs[0]);
+    char *ret_s = malloc(sizeof(char)*(strlen(strs[0])+1));
+    strcpy(ret_s,strs[0]);
+    int i, j;
+    for (i = 0; i < len_s; i++) {
+        for(j =0; j < strsSize-1; j++) {
+            if (strs[j][i] != strs[j+1][i]) {
+                ret_s[i] = '\0';
+                return ret_s;
+            }
+        }
+    }
+    ret_s[len_s] = '\0';
+    return ret_s;
+}
