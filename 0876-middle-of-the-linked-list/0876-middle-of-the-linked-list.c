ef73d7483bce15c6f4100f3994bf7f11f1ac0ae5
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-04-16 10:52:51 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.c
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* middleNode(struct ListNode* head) {
+    if (head == NULL|| head->next == NULL) return head;
+
+    struct ListNode* midnode = head, *endnode = head->next;
+    while (endnode != NULL) {
+        midnode = midnode->next;
+        endnode = endnode->next;
+        if (endnode) endnode = endnode->next;
+    }
+    return midnode;
+}
+
+=====================
+
+struct ListNode* middleNode(struct ListNode* head){
+    struct ListNode *fast = head, *slow = head;
+    while(fast && fast->next){
+        slow = slow->next;
+        fast = fast ->next ->next;
+    }
+    return slow;
+}
+/*
+作者：糗菇凉
+链接：https://leetcode.cn/problems/middle-of-the-linked-list/solutions/2105939/lian-biao-de-zhong-jian-jie-dian-by-qiu-kl0rd/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
