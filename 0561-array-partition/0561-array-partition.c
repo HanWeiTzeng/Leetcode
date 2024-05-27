d4731a11d14f65d02ce9223ef72f990f8fb39f95
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-05-27 23:26:44 +0800

    Time: 85 ms (6.24%), Space: 7.5 MB (49.01%) - LeetHub

--- 0561-array-partition/0561-array-partition.c
+++ 0561-array-partition/0561-array-partition.c
@@ -1,3 +1,5 @@
+
+/*
 void mergeSortedElement(int *nums, int left, int middle, int right) {
     int len_A = middle - left + 1;
     int len_B = right - (middle);
@@ -37,10 +39,48 @@ void mergesort(int *nums, int left, int right) {
         mergeSortedElement(nums, left, middle, right);
     }
 }
+*/
+void swap(int *a, int *b) {
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
 
+int partition(int *nums, int left, int right) {
+    int pivot = nums[right];
+    int i = left;
+    int j = right - 1;
+    while (i < j) {
+        while (i < right && nums[i] <= pivot) {
+            i++;
+        }
+        while (j > left && nums[j] >= pivot) {
+            j--;
+        }
+        if (i < j) {
+            swap(&nums[i], &nums[j]);
+        }
+    }
+
+    if (nums[i] > pivot) {
+        swap(&nums[i], &nums[right]);
+    }
+    return i;
+}
+void quickSort(int *nums, int left, int right) {
+    if (left < right) {
+        int pivot = partition(nums, left, right);
+        quickSort(nums, left, pivot-1);
+        quickSort(nums, pivot+1, right);
+    }
+}
 int arrayPairSum(int* nums, int numsSize) {
     int count = 0;
-    mergesort(nums, 0, numsSize-1);
+    //mergesort(nums, 0, numsSize-1);
+    quickSort(nums, 0, numsSize-1);
+    for (int i = 0; i < numsSize; i++) {
+        printf("%d ", nums[i]);
+    }
     for (int i = 0; i < numsSize; i += 2) {
         count += nums[i];
     }
