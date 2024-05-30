3897b8040373b28402dc8da7fdd489e409624c47
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-05-30 15:04:24 +0800

    Time: 11 ms (45.16%), Space: 7.9 MB (93.55%) - LeetHub

--- 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.c
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.c
@@ -10,6 +10,15 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
+ int count(struct Node* root) {
+    if (root == NULL) return 0;
+    int sum = 0;
+    for (int i = 0; i < root->numChildren; i++) {
+        sum += count(root->children[i]);
+    }
+    return 1 + sum;
+}
+
 void helper(struct Node* root, int* size_of_root, int *stack) {
     if (root == NULL) return;
     for (int i = 0; i < root->numChildren; i++) {
@@ -19,8 +28,9 @@ void helper(struct Node* root, int* size_of_root, int *stack) {
     *size_of_root += 1;
 }
 int* postorder(struct Node* root, int* returnSize) {
+    int sum = count(root);
     int size_root = 0;
-    int *ret_arr = (int *) malloc(sizeof(int) * 10000);
+    int *ret_arr = (int *) malloc(sizeof(int) * sum);
     helper(root, &size_root, ret_arr);
     *returnSize = size_root;
     return ret_arr;
