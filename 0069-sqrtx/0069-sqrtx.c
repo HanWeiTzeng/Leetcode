055256a916e1e63ce84f6b0495c824631c37026d
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-05-30 17:48:11 +0800

    Time: 5 ms (56.69%), Space: 5.3 MB (72.81%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.c
+++ 0069-sqrtx/0069-sqrtx.c
@@ -1,10 +1,9 @@
 int mySqrt(int x) {
-    if (x == 0) return 0;
+    if (x <= 1) return x;
     int left = 0;
     int right = x;
     while (left <= right) {
         int middle = (right+left)/2;
-        if (middle == 0) middle = 1;
         if (x/middle == middle && x%middle == 0) {return middle;}
         else if (middle > x/middle) {
             right = middle-1;
