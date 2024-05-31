b07bb94164a2024fcfdc9334a5bbbf22e90c3c98
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-05-31 16:42:13 +0800

    Time: 283 ms (7.38%), Space: 8.1 MB (58.47%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.c
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* mergeKLists(struct ListNode** lists, int listsSize) {
+    if (listsSize == 0) return NULL;
+    int idx = 0;
+    int null_flag = 1;
+    struct ListNode* head = lists[0];    // use 1st node, and set it as head in default.
+
+    // check all the first node in K lists.
+    for (int i = 0; i < listsSize; i++) {
+        //need to handle NULL pointer
+        if (lists[i] != NULL) {
+            null_flag = 0;
+            if (head == NULL || head->val > lists[i]->val) {
+                head = lists[i];
+                idx = i;
+            }
+        }
+    }
+    if (lists[idx] != NULL)
+    lists[idx] = lists[idx]->next;
+    if (null_flag == 0)
+    head->next = mergeKLists(lists, listsSize);
+    return head;
+}
