67388cbd986e229b943b55250b2123b7f90ae138
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-06-01 23:15:40 +0800

    Time: 77 ms (77.35%), Space: 12.3 MB (5.38%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.c
+++ 0053-maximum-subarray/0053-maximum-subarray.c
@@ -5,18 +5,14 @@ int max(int a, int b) {
 int maxSubArray(int* nums, int numsSize) {
     int max_subarray_sum = 0;
     int sum_pre = 0;
-    //if (i == 0) {
-            max_subarray_sum = nums[0];  
-    //} else 
+    max_subarray_sum = nums[0];  
     for (int i = 1; i < numsSize; i++) {
-        {
-            sum_pre += nums[i-1];
-            sum_pre = max(sum_pre, nums[i-1]);
-            if (nums[i] > 0 && sum_pre >= 0) {
-                max_subarray_sum = max(max_subarray_sum, sum_pre + nums[i]);
-            } else if (nums[i] > 0 && sum_pre < 0 || nums[i] <= 0) {
-                max_subarray_sum = max(max_subarray_sum, nums[i]);
-            }
+        sum_pre += nums[i-1];
+        sum_pre = max(sum_pre, nums[i-1]);
+        if (nums[i] > 0 && sum_pre >= 0) {
+            max_subarray_sum = max(max_subarray_sum, sum_pre + nums[i]);
+        } else if (nums[i] > 0 && sum_pre < 0 || nums[i] <= 0) {
+            max_subarray_sum = max(max_subarray_sum, nums[i]);
         }
     }
     return max_subarray_sum;
