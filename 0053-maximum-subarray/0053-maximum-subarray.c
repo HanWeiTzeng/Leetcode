4cc88242d6f969042c7a67cec4ace75092c8269f
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-06-01 22:37:51 +0800

    Time: 76 ms (81.77%), Space: 12 MB (58.34%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.c
+++ 0053-maximum-subarray/0053-maximum-subarray.c
@@ -1,40 +1,32 @@
 int maxSubArray(int* nums, int numsSize) {
-    int max_subarray_tv = 0;
+    int max_subarray_sum = 0;
     int idx = 0;
     int sum_pre = 0;
     int i = 0;
     for (int i = 0; i < numsSize; i++) {
         if (i == 0) {
-            max_subarray_tv = nums[i];  
-        } else if (nums[i] > 0) { //nums[i]=4, i=8
-            sum_pre += nums[i-1];
-            printf("sum_pre %d\n", sum_pre);
-            if (nums[i-1] > sum_pre) {
-                sum_pre = nums[i-1];
-                printf("Change sum_pre %d\n", sum_pre);
-            }
-            //for (int j = idx; j < i; j++) { //j=3, j<8, j++
-            //    sum_pre += nums[j];  //sum_pre = 1
-            //}
-            if (sum_pre < 0) {
-                if (nums[i] > max_subarray_tv) {
-                    max_subarray_tv = nums[i];  // 
-                    //idx = i;  // 3
-                }
-                idx = i;  // 3
-            } else if (sum_pre >= 0 && sum_pre + nums[i] > max_subarray_tv) {
-                max_subarray_tv = sum_pre + nums[i];  // max_subarray_tv=5+1=6  
-            }
+            max_subarray_sum = nums[i];  
         } else {
             sum_pre += nums[i-1];
             if (nums[i-1] > sum_pre) {
                 sum_pre = nums[i-1];
             }
-            if (nums[i] > max_subarray_tv) {
-                max_subarray_tv = nums[i];
-                idx = i;
+            if (nums[i] > 0) {
+                if (sum_pre < 0) {
+                    if (nums[i] > max_subarray_sum) {
+                        max_subarray_sum = nums[i];
+                    }
+                    idx = i;
+                } else if (sum_pre >= 0 && sum_pre + nums[i] > max_subarray_sum) {
+                    max_subarray_sum = sum_pre + nums[i];
+                }
+            } else { // if (nums[i] <= 0)
+                if (nums[i] > max_subarray_sum) {
+                    max_subarray_sum = nums[i];
+                    idx = i;
+                }
             }
         }
     }
-    return max_subarray_tv;
+    return max_subarray_sum;
 }
