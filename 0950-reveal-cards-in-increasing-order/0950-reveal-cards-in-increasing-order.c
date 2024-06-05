a156566f27e8c3de9112d041f5cce2da2c3b1208
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-06-06 00:58:37 +0800

    Time: 10 ms (71.56%), Space: 15.6 MB (6.51%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.c
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.c
@@ -30,12 +30,11 @@ int* deckRevealedIncreasing(int* deck, int deckSize, int* returnSize) {
     int i = 0;
     int *ret_arr = (int *)malloc(sizeof(int)*deckSize);
     while (idx_queue->front && i < deckSize) {
-        show_queue(idx_queue);
-        //printf("idx_queue->front->val %d i = %d",idx_queue->front->val, i);
         ret_arr[idx_queue->front->val] =  deck[i++];
         idx_queue->front = idx_queue->front->next;
         int tmp = dequeue(idx_queue);
         enqueue(idx_queue, tmp);
+        //show_queue(idx_queue);
     }
     return ret_arr;
 }
