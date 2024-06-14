8fc9a809f444859fc1269034503c43c0cdeda6f4
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-06-14 14:10:48 +0800

    Time: 202 ms (19.55%), Space: 60.8 MB (15.08%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.c
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.c
@@ -5,16 +5,44 @@
  *     struct ListNode *next;
  * };
  */
-struct ListNode* removeNodes(struct ListNode* head) {
-    // use recursive and find biggest.
-    if (head == NULL) return NULL;
-    struct ListNode* biggest_node = head;
-    while (head) {
-        if (head->val > biggest_node->val) {
-            biggest_node = head;
-        }
-        head = head->next;
+struct ListNode* reverse(struct ListNode* head) {
+    if (!head) return NULL;
+    struct ListNode* prev = NULL;
+    struct ListNode* curr = head;
+    while (curr) {
+        struct ListNode* tmp = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = tmp;
     }
-    biggest_node->next = removeNodes(biggest_node->next);
-    return biggest_node;
+    return prev;
+}
+struct ListNode* removeNodes(struct ListNode* head) {
+    // reverse node.
+    struct ListNode* dummynode = (struct ListNode*) malloc(sizeof(struct ListNode));
+    dummynode->val = 0;
+    dummynode->next = reverse(head);
+    struct ListNode* ret_listnode = dummynode;
+    struct ListNode* trav_listnode = ret_listnode;
+    struct ListNode* tmp;
+    int max = ret_listnode->val;
+
+    // keep the max value, and use it to judge.
+    while (trav_listnode) {
+        tmp = trav_listnode->next;
+        if (max > trav_listnode->val) { // 8 8
+            free(trav_listnode);
+        } else if (max <= trav_listnode->val) {
+            max = trav_listnode->val;
+            dummynode->next = trav_listnode;
+            dummynode = dummynode->next;
+        }
+        trav_listnode = tmp;
+    }
+    dummynode->next = NULL;
+    tmp = ret_listnode;
+    ret_listnode = ret_listnode->next;
+    free(tmp);
+    ret_listnode = reverse(ret_listnode);
+    return ret_listnode;
 }
