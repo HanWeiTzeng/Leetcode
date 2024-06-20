52da1a4fad15b1c66b3f8fd33f5bc42b5c1a3375
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-06-20 23:28:50 +0800

    Time: 103 ms (18.35%), Space: 21.7 MB (84.86%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.c
+++ 0189-rotate-array/0189-rotate-array.c
@@ -6,23 +6,22 @@ void rotate(int* nums, int numsSize, int k) {
     int pop_out = nums[i];     // 1
     int check_list[numsSize];
     memset(check_list, 0, sizeof(check_list));
-    //check_list[i] = 1;
+    check_list[i] = 1;
     // change a[i] to a[i+k] // remember % numsSize
     // use count to count total change times, need to equal to numsSize
     while (count <= numsSize) {
         i = (i+k) % numsSize;   // 3
-        int tmp2 = nums[i];    // 4
+        int tmp = nums[i];    // 4
         nums[i] = pop_out;   // 1
         if (check_list[i] == 0) {
-            pop_out = tmp2;    //tmp1 = 4
+            pop_out = tmp;    //tmp1 = 4
             check_list[i] = 1;
         } else {
             i = (i+1) % numsSize;
             if (check_list[i] == 1) return;
-            pop_out = nums[i];
+            pop_out = nums[i];  // don't pop out changed value
             check_list[i] = 1;
         }
-        printf("nums[%d] %d \n", i, nums[i]);
         count++;
     }
 }
