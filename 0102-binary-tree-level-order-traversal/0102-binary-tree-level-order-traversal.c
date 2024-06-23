945693689a792aaaa845759a8da3ce69c1bf9d35
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-06-23 09:08:50 +0800

    Time: 9 ms (53.28%), Space: 8.1 MB (77.29%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.c
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.c
@@ -18,20 +18,21 @@ int maxDepth(struct TreeNode* root) {
     return (left > right)? left : right; 
 }
 
-void helper(struct TreeNode* root, int* returnSize, int** returnColumnSizes, int level, int** ret_mat) {
+void addnum(int size, int **ret_mat, int val, int level)
+{
+    ret_mat[level] = realloc(ret_mat[level], size * sizeof (int));
+    ret_mat[level][size-1] = val;
+}
+
+void helper(struct TreeNode* root, int** returnColumnSizes, int level, int** ret_mat) {
     if (root == NULL) {
         return;
     }
-    if (level >= *returnSize) {
-        *returnSize = level + 1;
-    }
-    //deal with level 0
-    if ((*returnColumnSizes)[level] == 0) {
-        ret_mat[level] = (int *)malloc(2000 * sizeof(int));
-    }
-    ret_mat[level][(*returnColumnSizes)[level]++] = root->val;
-    helper(root->left, returnSize, returnColumnSizes, level+1, ret_mat);
-    helper(root->right, returnSize, returnColumnSizes, level+1, ret_mat);
+    (*returnColumnSizes)[level]++;
+    addnum((*returnColumnSizes)[level], ret_mat, root->val, level);
+    level++;
+    helper(root->left, returnColumnSizes, level, ret_mat);
+    helper(root->right, returnColumnSizes, level, ret_mat);
 }
 
 
@@ -41,10 +42,10 @@ int** levelOrder(struct TreeNode* root, int* returnSize, int** returnColumnSizes
         returnColumnSizes = NULL;
         return NULL;
     }
-    *returnSize = maxDepth(root);
-    int** ret_mat = (int **)malloc(sizeof(int*) * *returnSize);
-    *returnColumnSizes = (int *)calloc(*returnSize, sizeof(int));
-    *returnSize = 0;
-    helper(root, returnSize, returnColumnSizes, 0, ret_mat);
+    int depth = maxDepth(root);
+    *returnSize = depth;
+    int** ret_mat = calloc(depth, sizeof (int *));
+    *returnColumnSizes = (int *)calloc(depth, sizeof(int));
+    helper(root, returnColumnSizes, 0, ret_mat);
     return ret_mat;
 }
