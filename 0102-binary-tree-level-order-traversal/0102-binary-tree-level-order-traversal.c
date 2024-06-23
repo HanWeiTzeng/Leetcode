1e23055d8bc32e629563740cb7c9585a574f7417
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-06-23 08:36:57 +0800

    Time: 8 ms (56.77%), Space: 35.2 MB (27.95%) - LeetHub

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.c
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+void helper(struct TreeNode* root, int* returnSize, int** returnColumnSizes, int level, int** ret_mat) {
+    if (root == NULL) {
+        return;
+    }
+    if (level >= *returnSize) {
+        *returnSize = level + 1;
+    }
+    //deal with level 0
+    if ((*returnColumnSizes)[level] == 0) {
+        ret_mat[level] = (int *)malloc(2000 * sizeof(int));
+        //(*returnColumnSizes)[level] = 0;
+    }
+    ret_mat[level][(*returnColumnSizes)[level]++] = root->val;
+    helper(root->left, returnSize, returnColumnSizes, level+1, ret_mat);
+    helper(root->right, returnSize, returnColumnSizes, level+1, ret_mat);
+}
+
+int** levelOrder(struct TreeNode* root, int* returnSize, int** returnColumnSizes) {
+    if (root == NULL) {
+        *returnSize = 0;
+        returnColumnSizes = NULL;
+        return NULL;
+    }
+    int** ret_mat = (int **)malloc(sizeof(int*) * 2000);
+    *returnColumnSizes = (int *)malloc(sizeof(int *) * 2000);
+    for (int i = 0; i < 2000; i++) {
+        (*returnColumnSizes)[i] = 0;
+    }
+    *returnSize = 0;
+    int level = 0;
+    helper(root, returnSize, returnColumnSizes, level, ret_mat);
+    return ret_mat;
+}
