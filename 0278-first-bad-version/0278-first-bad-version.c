a69ff29fd4d07bd3e2c7f46c2c84c0136013f226
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-07-17 02:16:30 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.c
+++ 0278-first-bad-version/0278-first-bad-version.c
@@ -1,33 +1,26 @@
 // The API isBadVersion is defined for you.
 // bool isBadVersion(int version);
 
-// 二分法:
-
 int firstBadVersion(int n) {
-    int left = 1;
-    int right = n;
+    int start = 1;
+    int end = n;
+    int middle = (start + end) / 2;
+    int test1_version = isBadVersion(middle);
+    int test2_version = isBadVersion(middle + 1);
 
-    while (left < right) {
-        int middle = left + (right - left)/2;
-        if (isBadVersion(middle)) {
-            if (middle-left <= 1) {
-                if (isBadVersion(left)) return left;
-                    else return middle;
-            } else
-            right = middle;
-        } else {
-            left = middle+1;
+    while (!(test1_version == false && test2_version == true)) {
+        if (test1_version == false && test2_version == false) {
+            start = middle + 1;
+        } else if (test1_version == true && test2_version == true) {
+            end = middle - 1;
         }
-        middle = (left + right)/2;
+        middle = (start/2 + end/2);
+        if (start % 2 == 1 && end % 2 == 1) {
+            middle += 1;
+        }
+        //printf("middle %d\n", middle);
+        test1_version = isBadVersion(middle);
+        test2_version = isBadVersion(middle + 1);
     }
-    return left;
+    return middle + 1;
 }
-
-
-// 暴力:
-/*
-int firstBadVersion(int n) {
-    while (isBadVersion(n--));
-    return n+2;
-}
-*/
