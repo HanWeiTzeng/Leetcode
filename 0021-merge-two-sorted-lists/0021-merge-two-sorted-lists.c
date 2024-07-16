d8642c3dc57e71621ca639b057b132a1b097b3d4
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-07-16 16:24:55 +0800

    Time: 3 ms (59.73%), Space: 10.5 MB (7.9%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.c
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.c
@@ -31,15 +31,10 @@ struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
         head = head->next;
     }
     // add the rest of list1 or list2
-    while (list1 != NULL) {
-            head->next = list1;
-            list1 = list1->next;
-            head = head->next;
-    }
-    while (list2 != NULL) {
-            head->next = list2;
-            list2 = list2->next;
-            head = head->next;
+    if (list1) {
+        head->next = list1;
+    } else {
+        head->next = list2;
     }
     return ret_head->next;
 }
