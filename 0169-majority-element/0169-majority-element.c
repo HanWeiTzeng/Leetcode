71af4e9c31bc107d059cdfcd8cbcc106328ee343
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-07-17 03:53:16 +0800

    Time: 21 ms (45.63%), Space: 10 MB (7.04%) - LeetHub

--- 0169-majority-element/0169-majority-element.c
+++ 0169-majority-element/0169-majority-element.c
@@ -1,42 +1,16 @@
-//    哈希表可以用...
-
-//    異同互殺法:
-
 int majorityElement(int* nums, int numsSize) {
-    int most_num = nums[0];
     int count = 1;
+    int most_value = nums[0];
     for (int i = 1; i < numsSize; i++) {
-        if (count == 0) {
-            most_num = nums[i];
+        if (nums[i] == most_value) {
             count++;
-        } else if (most_num != nums[i]) {
-            count--;
         } else {
-            count++;
+            count--;
+            if (count < 0) {
+                count = 1;
+                most_value = nums[i];
+            }
         }
     }
-    return most_num;
+    return most_value;
 }
-
-/*
-=============================
-Quick sort法+ return nums[numsSize/2]
-
-int cmp(const void *e1, const void *e2) { =====> 更簡潔
-      return *(int*)e1 - *(int*)e2;
-}
-
-int compare(const void *a, const void *b) {
-      int c = *(int *)a;
-      int d = *(int *)b;
-      if(c < d) {return -1;}               //傳回 -1 代表 a < b
-      else if (c == d) {return 0;}      //傳回   0 代表 a = b
-      else return 1;                          //傳回  1 代表 a>b
-}
-
-
-int majorityElement(int* nums, int numsSize) {
-    qsort(nums, numsSize, sizeof(int), compare);
-    return nums[numsSize/2];
-}
-*/
