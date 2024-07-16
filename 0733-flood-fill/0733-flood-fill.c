6517206c3afafd411c91a2a1daf15c9ec3c1cfac
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-07-16 18:50:40 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.c
+++ 0733-flood-fill/0733-flood-fill.c
@@ -1,31 +1,27 @@
-
 /**
  * Return an array of arrays of size *returnSize.
  * The sizes of the arrays are returned as *returnColumnSizes array.
  * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
  */
 
-// 這題要注意 DFS 深度搜尋要另外寫才有辦法改
-
-
- void dfs(int** image, int imageSize, int* imageColSize, int sr, int sc, int newcolor, int oricolor) {
-    if (sr < 0 || sc < 0 || sr >= imageSize || sc >= imageColSize[0]) return;
-    if (image[sr][sc] == newcolor) return;
-    if (image[sr][sc] == oricolor) {
-        image[sr][sc] = newcolor;
-        dfs(image, imageSize, imageColSize, sr-1, sc, newcolor, oricolor);
-        dfs(image, imageSize, imageColSize, sr+1, sc, newcolor, oricolor);
-        dfs(image, imageSize, imageColSize, sr, sc+1, newcolor, oricolor);
-        dfs(image, imageSize, imageColSize, sr, sc-1, newcolor, oricolor);
+void dfs(int** image, int imageSize, int* imageColSize, int sr, int sc, int color, int original_color) {
+    if (sr >= imageSize || sc >= *imageColSize) {
+        return;
     }
-    return;
- }
+    if (image[sr][sc] == original_color) {
+        image[sr][sc] = color;
+        dfs(image, imageSize, imageColSize, sr, sc, color, image[sr][sc]);
+        dfs(image, imageSize, imageColSize, sr, sc, color, image[sr][sc]);
+        dfs(image, imageSize, imageColSize, sr, sc, color, image[sr][sc]);
+    }
+}
+
 int** floodFill(int** image, int imageSize, int* imageColSize, int sr, int sc, int color, int* returnSize, int** returnColumnSizes) {
-
     *returnSize = imageSize;
-    *returnColumnSizes = imageColSize; 
-    if (image[sr][sc] == color) return image;
-    dfs(image, imageSize, imageColSize, sr, sc, color, image[sr][sc]);
+    *returnColumnSizes = imageColSize;
 
+    if (image[sr][sc] == color) return image;
+
+    dfs(image, imageSize, imageColSize, sr, sc, color, image[sr][sc]);
     return image;
 }
