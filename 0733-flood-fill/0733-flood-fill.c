fc3d94270d2755771facdc4d3a4695a632b47fd9
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-07-16 18:51:59 +0800

    Time: 21 ms (41.88%), Space: 13.1 MB (8.55%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.c
+++ 0733-flood-fill/0733-flood-fill.c
@@ -5,18 +5,20 @@
  */
 
 void dfs(int** image, int imageSize, int* imageColSize, int sr, int sc, int color, int original_color) {
-    if (sr >= imageSize || sc >= *imageColSize) {
+    if (sr < 0 || sc < 0 || sr >= imageSize || sc >= imageColSize[0]) {
         return;
     }
     if (image[sr][sc] == original_color) {
         image[sr][sc] = color;
-        dfs(image, imageSize, imageColSize, sr, sc, color, image[sr][sc]);
-        dfs(image, imageSize, imageColSize, sr, sc, color, image[sr][sc]);
-        dfs(image, imageSize, imageColSize, sr, sc, color, image[sr][sc]);
+        dfs(image, imageSize, imageColSize, sr+1, sc, color, original_color);
+        dfs(image, imageSize, imageColSize, sr-1, sc, color, original_color);
+        dfs(image, imageSize, imageColSize, sr, sc+1, color, original_color);
+        dfs(image, imageSize, imageColSize, sr, sc-1, color, original_color);
     }
 }
 
 int** floodFill(int** image, int imageSize, int* imageColSize, int sr, int sc, int color, int* returnSize, int** returnColumnSizes) {
+    
     *returnSize = imageSize;
     *returnColumnSizes = imageColSize;
 
