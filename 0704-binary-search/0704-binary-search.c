d653a4d2bb0972b9550aa1a82bfea95fed89f3e0
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-07-16 17:15:45 +0800

    Time: 31 ms (12.91%), Space: 9.2 MB (7.54%) - LeetHub

--- 0704-binary-search/0704-binary-search.c
+++ 0704-binary-search/0704-binary-search.c
@@ -2,7 +2,8 @@ int search(int* nums, int numsSize, int target) {
     // check middle of array
     int i = 0, j = numsSize - 1;
     int middle = (i + j) / 2;
-    while (i < j) {
+    // divide it into 2 pieces by bigger than nums[middle] or smaller.
+    while (i <= j) {
         if (nums[middle] == target) {
             return middle;
         } else if (nums[middle] > target) {
@@ -12,11 +13,11 @@ int search(int* nums, int numsSize, int target) {
         }
         middle = (i + j) / 2;
     }
-    // divide it into 2 pieces by bigger than nums[middle] or smaller.
+
     // check the boundary
-    if (nums[i] == target) {
-        return i;
-    }
+    //if (nums[i] == target) {
+    //    return i;
+    //}
     // return -1 if hit boundary.
     return -1;
 }
