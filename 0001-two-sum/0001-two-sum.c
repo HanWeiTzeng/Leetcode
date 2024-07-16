3df9a32361ce7d6e151df0f2ebb503c83bf9bbe2
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-07-16 16:49:44 +0800

    Time: 111 ms (26.87%), Space: 8.8 MB (5.7%) - LeetHub

--- 0001-two-sum/0001-two-sum.c
+++ 0001-two-sum/0001-two-sum.c
@@ -5,19 +5,14 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     int *ret_arr = (int *)malloc(sizeof(int)*2);
     *returnSize = 2;
     // use 2 pointers i j to calculate sum
-    int i = 0, j = 1;
-    while (i < numsSize) {
-        while (j < numsSize) {
-            if (nums[i] + nums[j] != target) {
-                j++;
-            } else {
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = i+1; j < numsSize; j++) {
+            if (nums[i] + nums[j] == target) {
                 ret_arr[0] = i;
                 ret_arr[1] = j;
                 return ret_arr;
             }
         }
-        i++;
-        j = i+1;
     }
     return NULL;
 }
