7ca018586f6692a7a3b4574af7f8a736fc444e22
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-07-16 19:44:55 +0800

    Time: 7 ms (6.63%), Space: 8 MB (5.84%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.c
+++ 0020-valid-parentheses/0020-valid-parentheses.c
@@ -1,28 +1,43 @@
-char pair(char br) {
-    if (br == '}') return '{';
-    if (br == ')') return '(';
-    if (br == ']') return '[';
-    return '\0';
-}
-
 bool isValid(char* s) {
+    // create a stack, put ([{ and pop it when hit )]}
+    int str_len_s = strlen(s);
+    if (str_len_s % 2 == 1) {
+        return false;
+    }
+    int *stack_s = (int *)malloc(sizeof(int) * str_len_s);
     int i = 0;
     int j = 0;
-    char stack_b[strlen(s)/2+1];
-    while (i < strlen(s)) {
-        if (s[i] == '(' || s[i] == '{' || s[i] == '[') {     // {[( store in stack_b
-            stack_b[j++] = s[i];
-            if (j > strlen(s)/2) return false;
-        } else if (s[i] == ')' || s[i] == '}' || s[i] == ']') { // )]} change to {[( and check 
-            if (j == 0) return false;
-            if (stack_b[j-1] == pair(s[i])) {
-                j--;
-            } else {
+    while (i < str_len_s) {
+        printf("s[%d] = %c\n", i, s[i]);
+        if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
+            stack_s[j++] = s[i];
+        } else {
+            if (j <= 0) {
                 return false;
             }
-        } else return false;
+            if (s[i] == ')') {
+                if (stack_s[j-1] == '(') {
+                    j--;
+                } else {
+                    return false;
+                }
+            } else if (s[i] == ']') {
+                if (stack_s[j-1] == '[') {
+                    j--;
+                } else {
+                    return false;
+                }
+            } else if (s[i] == '}') {
+                if (stack_s[j-1] == '{') {
+                    j--;
+                } else {
+                    return false;
+                }
+            }
+        }
         i++;
     }
-    if (j == 0 && i == strlen(s)) return true;
-    else return false;
+    if (j != 0) return false;
+    //free(stack_s);
+    return true;
 }
