ccf573536aeb115cfade877576fa9f13d81c8b67
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-07-16 18:15:51 +0800

    Time: 33 ms (11.57%), Space: 27.9 MB (6.48%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.c
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.c
@@ -7,12 +7,18 @@
  * };
  */
 
-struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) {
-    if (root->val >= p->val && root->val <= q->val || root->val >= q->val && root->val <= p->val) {
+struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p,
+                                      struct TreeNode* q) {
+    if (root == p || root == q) {
         return root;
-    } else if (root->val > p->val && root->val > q->val ) {
-        return lowestCommonAncestor(root->left, p, q);
-    } else {
-        return lowestCommonAncestor(root->right, p, q);
     }
+
+    if (root->val > p->val && root->val > q->val) {
+        return lowestCommonAncestor(root->left, p, q);
+    } else if (root->val < p->val && root->val < q->val) {
+        return lowestCommonAncestor(root->right, p, q);
+    } else {
+        return root;
+    }
+
 }
