63d50cb0ea6ae17460480651ff1a7d602c285fef
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-07-16 17:02:03 +0800

    Time: 0 ms (100%), Space: 8.7 MB (5.28%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.c
+++ 0226-invert-binary-tree/0226-invert-binary-tree.c
@@ -10,10 +10,11 @@ struct TreeNode* invertTree(struct TreeNode* root) {
     // Handle root == NULL
     if (root == NULL) return NULL;
     // invert root->left and root->right
+    invertTree(root->right);
+    invertTree(root->left);
     struct TreeNode* tmp = root->left;
     root->left = root->right;
     root->right = tmp;
-    invertTree(root->right);
-    invertTree(root->left);
+
     return root;
 }
