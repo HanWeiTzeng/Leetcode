c90c0468e1887813782bb0fe779f76e3ee069c37
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-07-17 02:35:48 +0800

    Time: 0 ms (100%), Space: 7.8 MB (5.39%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.c
+++ 0070-climbing-stairs/0070-climbing-stairs.c
@@ -1,21 +1,18 @@
 int climbStairs(int n) {
-    int n1 = 1;
-    int n2 = 2;
-    for (int i = 3; i <= n; i++) {
-        if (i % 2 == 1) n1 = n1 + n2;
-        else  n2 = n1 + n2;
-    }
-    if (n % 2 == 1)
-        return n1;
-    else 
-        return n2;
-}
+    // n = 1 return 1;
+    // n = 2 return 2;
+    // n = 3 return 3;
+    // n = 4 return 5;
+    // n = 5 return 8;
+    if (n == 1 || n == 2)
+        return n;
 
- // Runtime Error !: Recursive:
-/*
-int climbStairs(int n) {
-    if (n == 0) return 0;
-    if (n == 1) return 1;
-    return climbStairs(n-1) + climbStairs(n-2);
+    int count1 = 2;
+    int count2 = 1;
+    for (int i = 2; i < n; i++) {
+        int tmp = count2;   // tmp = 1 2
+        count2 = count1;    // count2 = 2 3
+        count1 = count1 + tmp;  // count1 = 3 5
+    }
+    return count1;
 }
-*/
