cf3c6e8cc37077054705839c63ed585b91ab04d5
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-07-29 14:25:45 +0800

    Time: 13 ms (41.99%), Space: 13.8 MB (19.94%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.c
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.c
@@ -6,13 +6,19 @@
  *     struct TreeNode *right;
  * };
  */
-int subfunction(struct TreeNode* root, int *max) {
-    if (root == NULL) return 0;
+
+// 記得切開max 跟return value.
+// max 紀錄當前最長path, return value用來記錄當下node最長path
+
+int subfunction(struct TreeNode* root, int* max) {
+    if (root == NULL)
+        return 0;
 
     int left_length = subfunction(root->left, max);
     int right_length = subfunction(root->right, max);
     int total_length_tmp = left_length + right_length;
-    *max = fmax(*max, total_length_tmp);
+
+    *max = (*max > total_length_tmp) ? *max : total_length_tmp;
 
     return (left_length > right_length ? left_length : right_length) + 1;
 }
