25d49ac5b3b5bed47f8f748e35df3bb28ba73e77
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-07-30 12:28:12 +0800

    Time: 0 ms (100%), Space: 7.9 MB (16.93%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.c
+++ 0844-backspace-string-compare/0844-backspace-string-compare.c
@@ -1,31 +1,35 @@
-// ----->注意
-
-
-#include <string.h>
-
-char *helper (char* str) {
-    int s_len = strlen(str);
-    int len = 0;
-    char *ret = (char *) malloc (sizeof(char)*(s_len + 1));
+bool backspaceCompare(char* s, char* t) {
+    // use queue to store s & t
+    int s_len = strlen(s);
+    int t_len = strlen(t);
+    // when encountering # do index -1
+    char *queue_s = malloc(sizeof(char)* (s_len+1));
+    char *queue_t = malloc(sizeof(char)* (t_len+1));
+    // add \0 at index
+    int index_s = 0, index_t = 0;
     for (int i = 0; i < s_len; i++) {
-        if(str[i] != '#') {
-            ret[len++] = str[i];
+        if (s[i] != '#') {
+            queue_s[index_s++] = s[i];
         } else {
-            if (len > 0) {               //----->注意
-                len--;                      //----->注意
-            }
+            if (index_s != 0)
+                index_s--;
         }
     }
-    ret[len] = '\0';
-    return ret;
-}
+    queue_s[index_s] = '\0';
 
+    for (int i = 0; i < t_len; i++) {
+        if (t[i] != '#') {
+            queue_t[index_t++] = t[i];
+        } else {
+            if (index_t != 0)
+                index_t--;
+        }
+    }
+    queue_t[index_t] = '\0';
 
-bool backspaceCompare(char* s, char* t) {
-    return !(strcmp(helper(s), helper(t)));
-}
-
-
-bool backspaceCompare(char s, char t) {
-    return !(strcmp(helper(s), helper(t)));
+    // strcmp them.
+    if (strcmp(queue_s, queue_t) != 0) {
+        return false;
+    }
+    return true;
 }
