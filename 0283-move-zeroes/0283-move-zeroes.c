5af1719e86acb3d643ea0dcfadeafa7dd51c15ca
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-07-30 15:07:37 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.c
+++ 0283-move-zeroes/0283-move-zeroes.c
@@ -1,38 +1,23 @@
-//  1. 移動零:
-//  2. 直接後面補零:
-
-void swap (int *a, int *b) {
+void swap(int *a, int *b) {
     int tmp = *a;
     *a = *b;
     *b = tmp;
 }
 
 void moveZeroes(int* nums, int numsSize) {
-    int left = 0, right = 0;
-    while (right < numsSize) {
-        if (nums[right] != 0) {                                 // 右指標 不等於零才做
-            if (left != right)
-            swap(nums + left, nums + right);
-            left++;
+    int index_zero = 0, index_none_zero = 0;
+    // detect 0 and use swap to switch
+    while (index_zero < numsSize && index_none_zero < numsSize) {
+        /*while (nums[index_zero] != 0) {
+            printf("index_zero nums[%d] = %d\n", index_zero, nums[index_zero]);
+            index_zero++;
+        }*/
+        while (nums[index_none_zero] == 0 && index_none_zero < numsSize) {
+            printf("index_none_zero nums[%d] = %d\n", index_none_zero, nums[index_none_zero]);
+            index_none_zero++;
         }
-        right++;                                                         // 右指標 去找不是零的
+        swap(nums + index_zero, nums + index_none_zero);
+        index_zero++;
+        index_none_zero++;
     }
 }
-
-//  ============================
-//  2. 直接補零:
-/*
-void moveZeroes(int* nums, int numsSize) {
-    int len = 0;
-    for (int i = 0; i < numsSize; i++) {
-        if (nums[i] != 0) {
-            nums[len++] = nums[i];
-        }
-    }
-    if (len < numsSize) {
-        for (int j = len; j < numsSize; j++) {
-            nums[j] = 0;
-        }
-    }
-}
-*/
