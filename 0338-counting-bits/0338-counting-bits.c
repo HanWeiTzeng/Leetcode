9b6dbd07c7fc2885e3a4e535ddb2a1f72e8ad75c
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-07-30 13:53:40 +0800

    Time: 43 ms (25.63%), Space: 14.9 MB (19.31%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.c
+++ 0338-counting-bits/0338-counting-bits.c
@@ -1,24 +1,22 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-/*
-1. 利用 Brian Kernighan\text{Brian Kernighan}Brian Kernighan 算法，
-可以在一定程度上进一步提升计算速度。
-Brian Kernighan\text{Brian Kernighan}Brian Kernighan 算法的原理是：
-对于任意整数 x，令 x=x & (x−1)，逐步把所有的1變成0
-2. 也可以像這樣解
-*/
+int count_bit(int value) {
+    int count = 0;
+    while (value != 0) {
+        count++;
+        value &= (value -1);
+    }
+    return count;
+}
 
 int* countBits(int n, int* returnSize) {
-    int *ret_arr = (int *) malloc(sizeof(int)*(n + 1));
+    // create an array with size n
+    int* arr = (int*)malloc(sizeof(int) * (n + 1));
+    // use for loop for filling up with all element in array in order.
     for (int i = 0; i <= n; i++) {
-        int bw = i;
-        ret_arr[i] = bw & 1;
-        while (bw >> 1) {
-            ret_arr[i] += (bw >> 1) & 1;
-            bw = bw >> 1;
-        }
+        arr[i] = count_bit(i);
     }
     *returnSize = n + 1;
-    return ret_arr;
+    return arr;
 }
