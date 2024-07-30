bef07e8c2860fff344b5b63edc50f9f33204dcc0
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-07-30 11:38:31 +0800

    Time: 4 ms (9.3%), Space: 8.4 MB (12.65%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.c
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.c
@@ -6,29 +6,20 @@
  * };
  */
 struct ListNode* middleNode(struct ListNode* head) {
-    if (head == NULL|| head->next == NULL) return head;
-
-    struct ListNode* midnode = head, *endnode = head->next;
-    while (endnode != NULL) {
-        midnode = midnode->next;
-        endnode = endnode->next;
-        if (endnode) endnode = endnode->next;
+    // use first/ slow pointer.
+    if (head == NULL) {
+        return NULL;
     }
-    return midnode;
-}
-/*
-=====================
-
-struct ListNode* middleNode(struct ListNode* head){
-    struct ListNode *fast = head, *slow = head;
-    while(fast && fast->next){
-        slow = slow->next;
-        fast = fast ->next ->next;
+    struct ListNode* slow = head;
+    struct ListNode* fast = head->next;
+    while (fast != NULL) {
+        slow = slow->next;  // 1 2 3 
+        fast = fast->next;  // 2 3 4  5 null
+        if (fast && fast->next != NULL) {
+            fast = fast->next;  // 2->3 4 5 null
+        } else {
+            break;
+        }
     }
     return slow;
 }
-作者：糗菇凉
-链接：https://leetcode.cn/problems/middle-of-the-linked-list/solutions/2105939/lian-biao-de-zhong-jian-jie-dian-by-qiu-kl0rd/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
-*/
