912b1e6eff176ea70b7d6f89f92e64af93400dcb
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-07-30 14:03:54 +0800

    Time: 0 ms (100%), Space: 8.3 MB (9.21%) - LeetHub

--- 0100-same-tree/0100-same-tree.c
+++ 0100-same-tree/0100-same-tree.c
@@ -6,14 +6,18 @@
  *     struct TreeNode *right;
  * };
  */
- 
 bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
-    if (p == NULL && q== NULL) {
+    if (p == NULL && q == NULL) {
         return true;
-    } else if (p != NULL && q== NULL || p == NULL && q != NULL) {
-        return false;
-    } else if (p->val == q->val) {
-        return (isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
     }
+    if (p == NULL || q == NULL) {
+        return false;
+    }
+
+    if (p->val == q->val) {
+        // check its left and right.
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+
     return false;
 }
