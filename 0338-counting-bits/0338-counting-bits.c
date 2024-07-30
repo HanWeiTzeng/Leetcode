caf4a5e837309277cb160fe8a46f6e2bdda5cd30
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-07-30 14:00:07 +0800

    Time: 44 ms (19.31%), Space: 15.1 MB (8.54%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.c
+++ 0338-counting-bits/0338-counting-bits.c
@@ -13,10 +13,19 @@ int count_bit(int value) {
 int* countBits(int n, int* returnSize) {
     // create an array with size n
     int* arr = (int*)malloc(sizeof(int) * (n + 1));
+    *returnSize = n + 1;
+    arr[0] = 0;
     // use for loop for filling up with all element in array in order.
+    /*
     for (int i = 0; i <= n; i++) {
         arr[i] = count_bit(i);
     }
-    *returnSize = n + 1;
+    */
+
+    for (int i = 1; i <= n; i++) {
+        arr[i] = arr[i & (i-1)] + 1;
+        //printf("%d & %d = %d , arr = %d\n", i,i-1,i & (i-1),arr[i]);
+    }
+
     return arr;
 }
