7fa132719ad93f6c33e9d95799524167fb3edc4d
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-07-30 14:27:48 +0800

    Time: 14 ms (52.29%), Space: 9.6 MB (7.82%) - LeetHub

--- 0136-single-number/0136-single-number.c
+++ 0136-single-number/0136-single-number.c
@@ -1,37 +1,15 @@
-int singleNumber(int* nums, int numsSize) {
-    int count = 0;
-    for (int i = 0; i < numsSize; i++) {
-        count ^= nums[i];
-    }
-    return count;
+int cmp(const void *a, const void *b) {
+    return *(int *)a - *(int *)b;
 }
 
-
-/* Hash table method
-struct hashtable{
-    int key;
-    int count;
-    UT_hash_handle hh;
-};
-
 int singleNumber(int* nums, int numsSize) {
-    struct hashtable *ref = NULL;
-    struct hashtable *tmp;
-    for (int i = 0; i < numsSize; i++) {
-        HASH_FIND_INT(ref, nums+i, tmp);
-        if (tmp == NULL) {
-            tmp = malloc(sizeof(struct hashtable));
-            tmp->key = nums[i];
-            tmp->count = 1;
-            HASH_ADD_INT(ref, key, tmp);
-        } else {
-            tmp->count++;
+    qsort(nums, numsSize, sizeof(int), cmp);
+    int i = 0;
+    while (i < numsSize-1) {
+        if (nums[i] != nums[i+1]) {
+            return nums[i];
         }
+        i += 2;
     }
-    for (int i = 0; i < numsSize; i++) {
-        HASH_FIND_INT(ref, nums + i, tmp);
-        if (tmp->count == 1) return tmp->key;
-    }
-    return -1;
+    return nums[numsSize-1];
 }
-*/
