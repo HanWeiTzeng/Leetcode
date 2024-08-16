8609520d63d2b709c782ef32dc39f4ac5017a88b
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-08-16 11:48:13 +0800

    Time: 0 ms (100%), Space: 7.9 MB (10.64%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.c
+++ 0190-reverse-bits/0190-reverse-bits.c
@@ -1,27 +1,21 @@
-//  最聰明的方式：想看看!!
-
 uint32_t reverseBits(uint32_t n) {
-    uint32_t result = 0u;
-    for (int i = 0; i < 32; i++) {
-        result = (result << 1) + (n >> i & 1);
-    }
-    return result;
-}
-/*
-=====================================
-uint32_t reverseBits(uint32_t n) {
-    for (int i = 0; i < 16; i++) {
-        int c = n & (1u << i);
-        int a = ((n >> (31-i)) & 1u);
-        if (c != 0 && a == 0) {
-            n |= 1u << (31-i);
-            n &= ~(1u << i);
-        } else if (c == 0 && a != 0) {
-            n &= ~(1u << (31-i));
-            n |= 1u << i;
+    uint32_t ret_val = 0;
+    uint32_t _1_bits[32] = {0};
+    int index = 0;
+    while (n) {
+        if ((1 & n) == 1) {
+            _1_bits[index] = 1;
         }
-
+        index++;
+        n >>= 1;
     }
-    return n;
+
+    index--;
+
+    while (index >= 0) {
+        ret_val += _1_bits[index] << (31-index);
+        index--;
+    }
+
+    return ret_val;
 }
-*/
