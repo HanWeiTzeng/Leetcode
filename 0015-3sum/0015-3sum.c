f8a7f5827ff15a03be54c1cec22904affb587a56
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-08-16 15:27:19 +0800

    Time: 220 ms (58.34%), Space: 75.3 MB (5.17%) - LeetHub

--- 0015-3sum/0015-3sum.c
+++ 0015-3sum/0015-3sum.c
@@ -4,51 +4,49 @@
  * Note: Both returned array and *columnSizes array must be malloced, assume
  * caller calls free().
  */
-int cmp(const void *a, const void *b) {
-    return *(int *)a - *(int *)b;
-}
+int cmp(const void* a, const void* b) { return *(int*)a - *(int*)b; }
 
 int** threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSizes) {
-    int base = 200;
+    int base = 100;
     int** ret_matrix = (int**)malloc(sizeof(int*) * base);
     int sum = 0;
     *returnSize = 0;
     *returnColumnSizes = (int*)malloc(sizeof(int) * base);
     qsort(nums, numsSize, sizeof(int), cmp);
-
+    int j, k;
     // use 2 pointers i j to calculate sum
     for (int i = 0; i < numsSize; i++) {
-        if (i > 0 && nums[i] == nums[i-1]) {
+        if (i > 0 && nums[i] == nums[i - 1]) {
             continue;
         }
-        for (int j = i + 1; j < numsSize; j++) {
-            if (j > i+1 && nums[j] == nums[j-1]) {
-                continue;
-            }
-            for (int k = numsSize -1; k >= j + 1; k--) {
-                sum = nums[i] + nums[j] + nums[k];
-                if (k > j + 1 && nums[k] == nums[k-1]) {
-                    continue;
-                }
-    
-                if (sum < 0) {
-                    break;
-                } else if (sum == 0) {
-                    ret_matrix[*returnSize] = (int*)malloc(sizeof(int) * 3);
-                    (*returnColumnSizes)[*returnSize] = 3;
+        j = i + 1;
+        k = numsSize - 1;
+        while (j < k) {
+            sum = nums[i] + nums[j] + nums[k];
+            if (sum == 0) {
+                ret_matrix[*returnSize] = (int*)malloc(sizeof(int) * 3);
+                (*returnColumnSizes)[*returnSize] = 3;
 
-                    ret_matrix[*returnSize][0] = nums[i];
-                    ret_matrix[*returnSize][1] = nums[j];
-                    ret_matrix[*returnSize][2] = nums[k];
-                    *returnSize += 1;
-                    if (*returnSize == base) {
-                        base += 200;
-                        ret_matrix = (int **)realloc(ret_matrix, sizeof(int*) * base);
-                        *returnColumnSizes = (int*)realloc(*returnColumnSizes, sizeof(int) * base);
-                    }
+                ret_matrix[*returnSize][0] = nums[i];
+                ret_matrix[*returnSize][1] = nums[j];
+                ret_matrix[*returnSize][2] = nums[k];
+                *returnSize += 1;
+                if (*returnSize == base) {
+                    base += 100;
+                    ret_matrix =
+                        (int**)realloc(ret_matrix, sizeof(int*) * base);
+                    *returnColumnSizes =
+                        (int*)realloc(*returnColumnSizes, sizeof(int) * base);
                 }
-            }
+            // 记录符合要求的两个数，进行去重
+            int num1 = nums[j], num2 = nums[k];
+                while (nums[j] == num1 && j < k)
+                    j++;
+                while (nums[k] == num2 && j < k)
+                    k--;
+            } else if (sum < 0) j++;
+            else k--;
         }
     }
-    return ret_matrix;
+return ret_matrix;
 }
