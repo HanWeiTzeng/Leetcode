4680270f569e8ef01beeaa9daf8d7795a7b309bd
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-08-16 13:36:08 +0800

    Time: 20 ms (12.25%), Space: 14.5 MB (16.6%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.c
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.c
@@ -6,28 +6,22 @@
  *     struct TreeNode *right;
  * };
  */
-
-/*
-重點: 找到一樣的數字就跑看看是不是後面都一樣
-如果是return true
-不是的話，再把root->left 跟 root-> right帶進原式
-再找一輪，再不是，再找。
-*/
-
-bool checkIfSame(struct TreeNode* root, struct TreeNode* subRoot) {
+bool isSameTree(struct TreeNode* root, struct TreeNode* subRoot) {
     if (root == NULL && subRoot == NULL) return true;
-        else if (root == NULL && subRoot != NULL || root != NULL && subRoot == NULL) return false;
-    if (root->val != subRoot->val) return false;
-    return checkIfSame(root->left, subRoot->left) && checkIfSame(root->right, subRoot->right);
+    if (root == NULL ||subRoot == NULL) return false;
+    if (root->val == subRoot->val) {
+        return isSameTree(root->left, subRoot->left) && isSameTree(root->right, subRoot->right);
+    }
+    return false;
 }
 
 
 bool isSubtree(struct TreeNode* root, struct TreeNode* subRoot) {
-    if (root == NULL) return false;
-
+    if (root == NULL && subRoot == NULL) return true;
+    if (root == NULL ||subRoot == NULL) return false;
     if (root->val == subRoot->val) {
-        if (checkIfSame(root, subRoot)) return true;
+        bool check = isSameTree(root->left, subRoot->left) && isSameTree(root->right, subRoot->right);
+        if (check) return true; 
     }
-
     return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
 }
