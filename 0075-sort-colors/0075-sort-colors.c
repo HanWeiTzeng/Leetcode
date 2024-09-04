43742faeae0fda11989d6442af78d8fc7d755888
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-09-04 15:43:25 +0800

    Time: 16 ms (12.92%), Space: 9.9 MB (11.43%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.c
+++ 0075-sort-colors/0075-sort-colors.c
@@ -6,23 +6,32 @@ void swap(int *nums, int index_1, int index_2) {
 
 void sortColors(int* nums, int numsSize) {
     int ptr_left = 0, ptr_right = numsSize-1, ptr_iterating = 0;
-    while (ptr_left <= ptr_right && ptr_iterating <= numsSize-1) {
+    while (ptr_left <= ptr_right && ptr_iterating <= ptr_right) {
         while (ptr_left < numsSize -1 && nums[ptr_left] == 0) {
-            //printf("ptr_left %d+1.\n", ptr_left);
+            printf("ptr_left %d.\n", ptr_left+1);
             ptr_left++;
         }
         while (ptr_right >= 0 && nums[ptr_right] == 2) {
-            //printf("ptr_right %d-1.\n", ptr_right);
+            printf("ptr_right %d.\n", ptr_right-1);
             ptr_right--;
         }
 
         if (ptr_iterating < ptr_right && nums[ptr_iterating] == 2) {
-            //printf("swap 2 nums[%d] = %d, nums[%d] = %d.\n", ptr_iterating, nums[ptr_iterating], ptr_right, nums[ptr_right]);
+            printf("swap 2 nums[%d] = %d, nums[%d] = %d.\n", ptr_iterating, nums[ptr_iterating], ptr_right, nums[ptr_right]);
             swap(nums, ptr_iterating, ptr_right);
+            for (int i = 0; i < numsSize; i++) {
+                printf(" %d", nums[i]);
+            }
+            printf("\n");
         } else if (ptr_iterating > ptr_left && nums[ptr_iterating] == 0) {
-            //printf("swap 0 nums[%d] = %d, nums[%d] = %d.\n", ptr_left, nums[ptr_left], ptr_iterating, nums[ptr_iterating]);
+            printf("swap 0 nums[%d] = %d, nums[%d] = %d.\n", ptr_left, nums[ptr_left], ptr_iterating, nums[ptr_iterating]);
             swap(nums, ptr_left, ptr_iterating);
+            for (int i = 0; i < numsSize; i++) {
+                printf(" %d", nums[i]);
+            }
+            printf("\n");
         } else {
+            printf("ptr_iterating = %d.\n", ptr_iterating+1);
             ptr_iterating++;
         }
     }
