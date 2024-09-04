5b6d82de412f74d45feec4568c207daeb692f2ba
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-09-04 15:32:25 +0800

    Time: 0 ms (100%), Space: 9.7 MB (21.73%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.c
+++ 0075-sort-colors/0075-sort-colors.c
@@ -1,21 +1,29 @@
+void swap(int *nums, int index_1, int index_2) {
+    int tmp = nums[index_1];
+    nums[index_1] = nums[index_2];
+    nums[index_2] = tmp;
+}
+
 void sortColors(int* nums, int numsSize) {
-    int count_amounts[3] = {0};
-    for (int i = 0; i < numsSize; i++) {
-        if (nums[i] == 0)
-            count_amounts[0]++;
-        else if (nums[i] == 1)
-            count_amounts[1]++;
-        else if (nums[i] == 2)
-            count_amounts[2]++;
-    }
-    int i = 0;
-    while (i < count_amounts[0]) {
-        nums[i++] = 0;
-    }
-    while (i < count_amounts[1] + count_amounts[0]) {
-        nums[i++] = 1;
-    }
-    while (i < count_amounts[2] + count_amounts[1] + count_amounts[0]) {
-        nums[i++] = 2;
+    int ptr_left = 0, ptr_right = numsSize-1, ptr_iterating = 0;
+    while (ptr_left <= ptr_right && ptr_iterating <= numsSize-1) {
+        while (ptr_left < numsSize -1 && nums[ptr_left] == 0) {
+            printf("ptr_left %d+1.\n", ptr_left);
+            ptr_left++;
+        }
+        while (ptr_right >= 0 && nums[ptr_right] == 2) {
+            printf("ptr_right %d-1.\n", ptr_right);
+            ptr_right--;
+        }
+
+        if (ptr_iterating < ptr_right && nums[ptr_iterating] == 2) {
+            printf("swap 2 nums[%d] = %d, nums[%d] = %d.\n", ptr_iterating, nums[ptr_iterating], ptr_right, nums[ptr_right]);
+            swap(nums, ptr_iterating, ptr_right);
+        } else if (ptr_iterating > ptr_left && nums[ptr_iterating] == 0) {
+            printf("swap 0 nums[%d] = %d, nums[%d] = %d.\n", ptr_left, nums[ptr_left], ptr_iterating, nums[ptr_iterating]);
+            swap(nums, ptr_left, ptr_iterating);
+        } else {
+            ptr_iterating++;
+        }
     }
 }
