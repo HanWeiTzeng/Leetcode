0537f9c6c56910ac481f59d230722e8e446957f3
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-10-08 22:25:02 +0800

    Time: 12 ms (43.22%), Space: 14.1 MB (26.18%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.c
+++ 0039-combination-sum/0039-combination-sum.c
@@ -1,47 +1,44 @@
 /**
  * Return an array of arrays of size *returnSize.
  * The sizes of the arrays are returned as *returnColumnSizes array.
- * Note: Both returned array and *columnSizes array must be malloced, assume
- * caller calls free().
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
  */
-
+int *ansColumnSize;
 int ansSize;
-int* ansColumnSize;
 
-void helper(int* candidates, int candidatesSize, int index, int target, int **ans, int *tmp_arr, int count) {
-    if (index == candidatesSize) {
+void dfs(int* candidates, int candidatesSize, int target, int** ret_ans, int index, int count, int* tmp_arr) {
+    // deal with target < 0 :done
+    if (target < 0 || index == candidatesSize) {
         return;
-    }
-    if (target == 0) {
-        ans[ansSize] = (int *) malloc(sizeof(int) * count);
-        for (int i = 0; i < count; ++i) {
-            ans[ansSize][i] = tmp_arr[i];
+    } else if (target == 0) {
+        // deal with target = 0: TBD
+        ansColumnSize[ansSize] = count;
+        ret_ans[ansSize] = (int *) malloc(sizeof(int)* count);
+        for (int i = 0; i < count; i++) {
+            ret_ans[ansSize][i] = tmp_arr[i];
         }
-        ansColumnSize[ansSize++] = count;
-        //printf(" count = %d, ", count_1);
-        //printf("Finded.\n");
+        ansSize++;
         return;
     }
-    helper(candidates, candidatesSize, index+1, target, ans, tmp_arr, count);
 
-    int new_target = target - candidates[index];
-    if (new_target >= 0) {
+    // deal with target > 0: done
+    int newtarget = target - candidates[index];
+    if (newtarget >= 0) {
         tmp_arr[count] = candidates[index];
-        //printf("target = %d. candidates[%d] = %d. count = %d\n", target, index, candidates[index], count);
-        helper(candidates, candidatesSize, index, new_target, ans, tmp_arr, count+1);
+        dfs(candidates, candidatesSize, newtarget, ret_ans, index, count+1, tmp_arr);
     }
-
+    dfs(candidates, candidatesSize, target, ret_ans, index+1, count, tmp_arr);
 }
 
-int** combinationSum(int* candidates, int candidatesSize, int target,
-                     int* returnSize, int** returnColumnSizes) {
-    // use the first candidates element, and minize it from target.
-    int** ans = malloc(sizeof(int*) * 1001);
+
+int** combinationSum(int* candidates, int candidatesSize, int target, int* returnSize, int** returnColumnSizes) {
+    int** ret_ans = (int**) malloc(sizeof(int*) * 1001);
     ansColumnSize = malloc(sizeof(int) * 1001);
-    int tmp_arr[40];
     ansSize = 0;
-    helper(candidates, candidatesSize, 0, target, ans, tmp_arr, 0);
+    int tmp_arr[40];
+    // create an array to store temporate array which is candicate
+    dfs(candidates, candidatesSize, target, ret_ans, 0, 0, tmp_arr); //index;
     *returnSize = ansSize;
     *returnColumnSizes = ansColumnSize;
-    return ans;
+    return ret_ans;
 }
