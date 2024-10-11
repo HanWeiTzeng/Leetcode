47ed00732c3182855bdc70a230155a94c63707fe
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-10-11 12:59:05 +0800

    Time: 16 ms (6.34%), Space: 11.4 MB (88.73%) - LeetHub

--- 0046-permutations/0046-permutations.c
+++ 0046-permutations/0046-permutations.c
@@ -3,7 +3,7 @@
  * The sizes of the arrays are returned as *returnColumnSizes array.
  * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
  */
-// Function to swap two elements in an array
+
 void swap(int *x, int *y) {
     int temp = *x;
     *x = *y;
@@ -12,19 +12,20 @@ void swap(int *x, int *y) {
 
 void bfs(int* array, int end, int start, int **ret_2d_arr, int* returnSize, int** returnColumnSizes) {
 // Function to generate all permutations of the array
-    if (start == end-1) {
+    if (start == end - 1) {
         ret_2d_arr[*returnSize] = (int *) malloc(sizeof(int) * end);
         (*returnColumnSizes)[*returnSize] = end;
         for (int i = 0; i < end; i++) {
             ret_2d_arr[*returnSize][i] = array[i];
-            //printf("%d ", ret_2d_arr[*returnSize][i]);
+            printf("%d ", ret_2d_arr[*returnSize][i]);
         }
+        printf("\n");
         *returnSize += 1;
     } else {
         for (int i = start; i < end; i++) {
             // Swap the current element with the start
+            printf("array[start(%d)] = %d, array[i(%d)] = %d.\n", start, array[start], i, array[i]);
             swap(&array[start], &array[i]);
-
             // Recursively call permute for the remaining elements
             bfs(array, end, start + 1, ret_2d_arr, returnSize, returnColumnSizes);
 
