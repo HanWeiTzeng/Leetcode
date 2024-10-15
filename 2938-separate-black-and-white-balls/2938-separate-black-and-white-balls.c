111c4a4e1cb22c6f84126d7269e13ba541b51d74
HanWeiTzeng <166488354+HanWeiTzeng@users.noreply.github.com>
2024-10-15 10:41:52 +0800

    Time: 24 ms (16.67%), Space: 10.7 MB (8.33%) - LeetHub

--- 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.c
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.c
@@ -11,14 +11,13 @@ long long minimumSteps(char* s) {
     int rightest_zero_position[100000];
     int count_zero = -1;
     int count_zero_head = 0;
-    int size = 0;
     int i = str_len_s-1;
     while (i >= 0) {
         //printf("s[%d] = %c.\n", i, s[i]);
         // recording the pointer to the rightest 0.
         if (s[i] == '1') {      //
             // check if count == -1, if not swap 1 and 0.
-            if (size != 0) {
+            if (count_zero != -1) {
                 steps += (rightest_zero_position[count_zero_head] - i);
                 //printf("swap %d %d.\n", i, rightest_zero_position[count_zero]);
                 swap(s + i, s + rightest_zero_position[count_zero_head++]);
@@ -28,7 +27,6 @@ long long minimumSteps(char* s) {
         } else if (s[i] == '0'){
             // record its index;
             rightest_zero_position[++count_zero] = i;
-            size++;
         }
         i--;
     }
